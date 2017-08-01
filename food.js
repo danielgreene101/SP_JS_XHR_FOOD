7d82dc4c6ed6317cd0289c592eed7507025cc404
Daniel Greene <d.b.greene101@gmail.com>
2017-08-01 16:58:57 -0500

    finished

--- food.js
+++ food.js
@@ -7,14 +7,45 @@ xhttp.onreadystatechange = function() {
         var result = JSON.parse(xhttp.responseText).dog_brands;
 //        console.log('here', result);
         for (var i = 0; i < result.length; i++){
-            var brand = document.createElement("h1");
+            var brand = document.createElement("h3");
             brand.innerHTML = result[i].name;
             placeToGo.appendChild(brand);
-//            var tp = result.types;
-//            console.log('here', tp);
+            
+            var tp = result[i].types;
+            tp.forEach(function(element) {
             var foodtype = document.createElement("li");
-            foodtype.innerHTML = result[i].type;
+            foodtype.innerHTML = element.type;
+                console.log("volumes", element.volumes)
             placeToGo.appendChild(foodtype);
+            
+                
+//            for (var t = 0; t < result.length; t++){
+//                console.log('result', result.length);
+//            var vol = tp[t].volumes;
+//                console.log('vol', vol);
+                
+                
+                
+//                for (var g = 0; g < result.length; g++){
+//                var down = vol[g].name;
+//                console.log('down', down);
+//                var up = vol[g].price;
+//                    console.log('up', up);
+////                console.log('down', down);
+               
+                    
+            
+                 element.volumes.forEach(function(element){
+//                    console.log("element", element);
+                var volume = document.createElement('p');
+                volume.innerHTML += element.name + ' ' + element.price;
+                placeToGo.appendChild(volume);
+                })
+            }
+            
+                
+            
+            )
         }
         
     }
@@ -24,15 +55,53 @@ xhttp.send();
 
 
 
-//var xhttp2 = new XMLHttpRequest();
-//xhttp2.onreadystatechange = function() {
-//    if (this.readyState == 4 && this.status == 200) {
-//        JSON.parse(xhttp2.responseText)
-//        let responses = xhttp2.responseText;
-//        let foods = responses;
-//        document.getElementById('cat').innerHTML += foods;
-//    }
-//};
-//
-//xhttp2.open("GET", "cat.JSON", true);
-//xhttp2.send();
+var xhttp2 = new XMLHttpRequest();
+xhttp2.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        
+        var placeToGo = document.getElementById('cat');
+        var result = JSON.parse(xhttp2.responseText).cat_brands;
+//        console.log('here', result);
+        for (var i = 0; i < result.length; i++){
+            var brand = document.createElement("h3");
+            brand.innerHTML = result[i].name;
+            placeToGo.appendChild(brand);
+            
+            var tp = result[i].Breeds;
+            tp.forEach(function(element) {
+            var foodtype = document.createElement("li");
+            foodtype.innerHTML = element.Breed;
+            placeToGo.appendChild(foodtype);
+            
+                
+//            for (var t = 0; t < result.length; t++){
+//            var vol = tp[t].volumes;
+////                console.log('vol', vol);
+//                
+//                
+//                for (var g = 0; g < vol.length; g++){
+//                var down = vol[g].type;
+////                console.log('down', down);
+//                var up = vol[g].price;
+////                    console.log('up', up);
+////                console.log('down', down);
+//                var side = vol[g].size;
+                element.volumes.forEach(function(element){
+                    
+                var volume = document.createElement('p');
+                volume.innerHTML += element.type + ' ' + element.size + ' ' + element.price;
+                placeToGo.appendChild(volume);
+                })
+                })
+                       }
+            }
+                
+            
+
+        
+        
+
+};
+
+xhttp2.open("GET", "cat.JSON", true);
+xhttp2.send();
